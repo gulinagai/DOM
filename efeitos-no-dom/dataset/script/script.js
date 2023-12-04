44e4a41624c21cdb824e99ddbda16c137b5227b8
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-04 10:27:41 -0300

    dataset atributes

--- efeitos-no-dom/dataset/script/script.js
+++ efeitos-no-dom/dataset/script/script.js
@@ -4,3 +4,17 @@ function Naosei(){
 }
 
 const nsei = new Naosei()
+
+ const phtml = document.querySelector('p')
+
+ const pjs = document.createElement('p')
+
+ console.log(pjs.constructor.name)
+ console.log(Object.getPrototypeOf(pjs))
+
+ console.log(HTMLElement.hasOwnProperty('dataset'))
+
+ console.log(Object.getPrototypeOf(HTMLParagraphElement.prototype))
+
+ const div = document.querySelector('div')
+ console.log(div.dataset)
