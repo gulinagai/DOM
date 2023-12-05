1818bc7c32a34dcb5639d3aa22032a715e05e2b8
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-05 14:19:38 -0300

    introducao a modulos

---
+++ efeitos-no-dom/module-introducao/js/modules/teste.js
@@ -0,0 +1,12 @@
+export function teste1(){
+    console.log('testando')
+}
+function teste2(){
+    console.log('esse teste não vai aparecer')
+}
+export function teste3(){
+    console.log('teste na outra função')
+}
+function acessopelometodo(){
+    console.log('Acessando pelo método sem saber o nome do item')
+}
