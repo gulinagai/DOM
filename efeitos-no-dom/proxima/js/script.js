5d65b10227ec95d5e64035e36781cb19894e5e12
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-06 16:29:13 -0300

    modules

---
+++ efeitos-no-dom/proxima/js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import animacaodoscroll from './modules/scroll-animacao.js'
+import iniciarnavaccordionlist from './modules/accordion.js'
+import iniciarnavtab from './modules/tabnav.js'
+
+
+initScrollSuave()
+animacaodoscroll()
+iniciarnavaccordionlist()
+iniciarnavtab()
+
