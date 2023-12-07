25d907abc212c7c9daf6fb5105e1c566ecd68f67
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-07 17:14:32 -0300

    tooltip html css js

--- efeitos-no-dom/tooltip/js/script.js
+++ efeitos-no-dom/tooltip/js/script.js
@@ -3,10 +3,12 @@ import animacaodoscroll from './modules/scroll-animacao.js'
 import iniciarnavaccordionlist from './modules/accordion.js'
 import iniciarnavtab from './modules/tabnav.js'
 import iniciarmodal from './modules/modal.js'
+import iniciartooltip from './modules/tooltip.js'
 
 initScrollSuave()
 animacaodoscroll()
 iniciarnavaccordionlist()
 iniciarnavtab()
 iniciarmodal()
+iniciartooltip()
 
