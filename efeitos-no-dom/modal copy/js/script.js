dfd93639b44cecc4f2039ce68ccc68df45acb0a6
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-07 12:05:01 -0300

    adicionei modal ao site

--- efeitos-no-dom/modal copy/js/script.js
+++ efeitos-no-dom/modal copy/js/script.js
@@ -2,10 +2,11 @@ import initScrollSuave from './modules/scroll-suave.js'
 import animacaodoscroll from './modules/scroll-animacao.js'
 import iniciarnavaccordionlist from './modules/accordion.js'
 import iniciarnavtab from './modules/tabnav.js'
-
+import iniciarmodal from './modules/modal.js'
 
 initScrollSuave()
 animacaodoscroll()
 iniciarnavaccordionlist()
 iniciarnavtab()
+iniciarmodal()
 
