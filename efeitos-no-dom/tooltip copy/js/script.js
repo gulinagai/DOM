3fc831e9e98958836f4ceeca8aa4bbf09c8b4c5c
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-08 11:11:41 -0300

    começando dropdown menu

---
+++ efeitos-no-dom/tooltip copy/js/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import animacaodoscroll from './modules/scroll-animacao.js'
+import iniciarnavaccordionlist from './modules/accordion.js'
+import iniciarnavtab from './modules/tabnav.js'
+import iniciarmodal from './modules/modal.js'
+import iniciartooltip from './modules/tooltip.js'
+
+initScrollSuave()
+animacaodoscroll()
+iniciarnavaccordionlist()
+iniciarnavtab()
+iniciarmodal()
+iniciartooltip()
+
