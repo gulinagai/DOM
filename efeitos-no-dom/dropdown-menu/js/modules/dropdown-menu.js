0be56a01503b9b966bd3d0ce1875e0c6cc197314
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-09 00:29:07 -0300

    exercicio dropdown menu

--- efeitos-no-dom/dropdown-menu/js/modules/dropdown-menu.js
+++ efeitos-no-dom/dropdown-menu/js/modules/dropdown-menu.js
@@ -2,3 +2,15 @@ export default function iniciarDropdownMenu(){
     
 }
   
+const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+dropdownMenus.forEach((menu)=>{
+    ['touchstart', 'click'].forEach((tipoDeEvento)=>{
+        menu.addEventListener(tipoDeEvento, handleClick)
+    })
+})
+
+function handleClick (event){
+    event.preventDefault()
+    console.log(event)
+    this.classList.toggle('active')
+}
